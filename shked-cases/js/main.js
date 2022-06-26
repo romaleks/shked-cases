1e648b781af8cffad2a7fbdb0f8ac37ecb3b8ac1
IDDna <romanov1371@gmail.com>
2022-06-26 17:26:15 +0300

    Create main page

---
+++ shked-cases/js/main.js
@@ -0,0 +1,28 @@
+
+
+function ibg() {
+   $.each($('.ibg'), function (index, val) {
+      if ($(this).find('img').length > 0) {
+         $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+      }
+   });
+}
+ibg();
+//SLIDERS
+if($('.slider__body').length>0){
+   $('.slider__body').slick({
+      //autoplay: true,
+      //infinite: false,
+      dots: true,
+      arrows: false,
+      accessibility:false,
+      slidesToShow:1,
+      autoplaySpeed: 3000,
+      nextArrow:'<button type="button" class="slick-next"></button>',
+      prevArrow:'<button type="button" class="slick-prev"></button>',
+      responsive: [{
+         breakpoint: 768,
+         settings: {}
+      }]
+   });
+}
